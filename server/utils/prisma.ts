589340cdf7cac142dcc5debb5e8ed9f2cf5f517c
Armel Cloarec <armel@armelclo.fr>
2026-01-14 18:52:57 +0100

    fix: schema and db connection

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -2,11 +2,12 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaMariaDb({
-  user: "root",
-  password: "my-secret-pw",
-  host: "localhost",
-  port: 3306,
-  database: "dev3",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
   connectTimeout: 5000,
+  allowPublicKeyRetrieval: true,
 });
 export const prisma = new PrismaClient({ adapter });
